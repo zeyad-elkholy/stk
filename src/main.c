31f0d6131f3e18680c4f230997fa2c299b71ef0f
zod <zeyadelkholy7@gmail.com>
2026-08-16 15:27:40 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -18,11 +18,11 @@ int main(int argc, char *argv[]) {
       continue;
     }else if (strncmp(command, "type ",5) == 0){
 if (strcmp(command + 5, "echo") == 0) {
-        printf("echo: shell builtin\n");
+        printf("echo is a shell builtin\n");
       } else if (strcmp(command + 5, "type") == 0) {
-        printf("type: shell builtin\n");
+        printf("type is a shell builtin\n");
       } else if (strcmp(command + 5, "exit") == 0) {
-        printf("exit: shell builtin\n");
+        printf("exit is a shell builtin\n");
       } else {
         printf("%s: command not found\n", command + 5);
       }
